dbfb83f70b85a16702302b2c861e39b3ae01c4b6
Heather Wilson <hdaniwilson@gmail.com>
2021-04-10 19:50:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,21 +21,3 @@ def won?(board)
     position_taken?(board, combo[0])
   end
 end
-
-def full?(board)
-  board.all?{|postion| postion == "X" || postion == "O"}
-end
-
-def draw(board)
-  full?(board) && !won?(board)
-end
-
-def over?(board)
-  won?(board) || full?(board)
-end
-
-def winner(board)
-  if winning_combo == won?(board)
-    board[winning_combo.first]
-  end
-end
